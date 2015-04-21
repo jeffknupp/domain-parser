894b63b6332f2cc42d9967bc7c49c98bc6dbff2e
Jeff Knupp <jknupp@appnexus.com>
2015-04-21 12:13:48 -0400

    Fix name

--- update_version.sh
+++ update_version.sh
@@ -1,9 +1,7 @@
-git flow release start v$1
-sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" sandman/__init__.py
+sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" domain_parser/__init__.py
 rm -rf docs/generated
 python setup.py develop
 make docs
-git commit docs sandman/__init__.py -m "Update to version v$1"
-git flow release finish v$1
+git commit docs domain_parser/__init__.py -m "Update to version v$1"
 python setup.py sdist upload -r pypi
 python setup.py upload_docs -r pypi
